3fef8420c0abc19afa956bc7381219e80a88b052
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-08-01 20:37:12 -0700

    order users by date created ascending

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -4,8 +4,13 @@ from database import Cursor, Database as db
 def get_users(page_size, page_index):
   try:
     with Cursor() as cur:
-      cur.execute("SELECT * FROM users WHERE type = 'regular' LIMIT %s OFFSET %s",
-        (page_size, page_size * page_index))
+      cur.execute(
+        "SELECT * FROM users "
+        "WHERE type = 'regular' "
+        "ORDER BY created_by ASC "
+        "LIMIT %s OFFSET %s",
+        (page_size, page_size * page_index)
+      )
 
       if cur.rowcount:
         return server.ok(data=cur.fetchall())
