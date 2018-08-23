4f9bcde0c6acb8e09d5de6cbe534027c41a4607e
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-08-23 11:15:52 -0700

    add user id claim to jwt

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -15,8 +15,12 @@ def login(email, password, auth_type):
         data['user'] = user
 
         if bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
-          data['auth_token'] = jwt.encode({}, os.environ.get('JWT_SIGNING_KEY'),
-            algorithm='HS256').decode('utf-8')
+          data['auth_token'] = jwt.encode(
+            {'user_id': user['id']},
+            os.environ.get('JWT_SIGNING_KEY'),
+            'HS256'
+          ).decode('utf-8')
+
           return server.ok(data=data)
         else:
           return server.bad_req('incorrect password')
