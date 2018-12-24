e05add7402985f83cad989fa2945d8f1ff50a969
Grapevine PD <grapevinepd@bender.cs.csubak.edu>
2018-12-24 11:06:14 -0800

    fix emailing, modify template

--- emails.py
+++ emails.py
@@ -2,14 +2,16 @@ import os
 import server
 import smtplib
 
-from utils import read_template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from io import open
+from string import Template
 
 SMTP_HOST = os.environ.get('SMTP_HOST')
 SMTP_PORT = os.environ.get('SMTP_PORT')
 SMTP_EMAIL = os.environ.get('SMTP_EMAIL')
 SMTP_PASSWORD = os.environ.get('SMTP_PASSWORD')
+print(SMTP_HOST, SMTP_PORT, SMTP_EMAIL, SMTP_PASSWORD)
 
 def new_user(email, password):
   message = MIMEMultipart()
@@ -17,11 +19,21 @@ def new_user(email, password):
   message['To'] = email
   message['Subject'] = 'Welcome to GrapevinePD'
 
-  new_user_template = read_template('email_templates/new_user.html')
-  message_body = new_user_template.substitute(EMAIL=email, PASSWORD=password)
+  f = open('email_templates/new_user.html', encoding='utf-8')
+  new_user_template = Template(f.read())
+
+  values = {
+    'EMAIL': email,
+    'PASSWORD': password
+  }
+  message_body = new_user_template.substitute(values)
   message.attach(MIMEText(message_body, 'html'))
 
-  smtp = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
+  smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+  smtp.ehlo()
   smtp.starttls()
+  smtp.ehlo()
   smtp.login(SMTP_EMAIL, SMTP_PASSWORD)
-  smtp.send_message(message)
+
+  smtp.sendmail('noreplay@grapevinepd.csub@gmail.com', email, message.as_string())
+  smtp.quit()
