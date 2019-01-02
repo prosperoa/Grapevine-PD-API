9ae5002bf1d0d0b1cfe5c11468389edfce455b79
Grapevine PD <grapevinepd@bender.cs.csubak.edu>
2019-01-02 11:46:59 -0800

    fix image parsing

--- handlers/image_handler.py
+++ handlers/image_handler.py
@@ -1,8 +1,16 @@
-import os
+import server
 import sys
+
 sys.path.append('controllers')
 import image_controller
 
 def process_image(req, alexnet):
-  img = req.files['image']
+  img = None
+
+  try:
+    img = req.files['image']
+  except:
+    return server.bad_req()
+
   return image_controller.analyze_image(img, alexnet)
+
